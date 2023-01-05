34826f7131b07c18fe69ff1724929eb7f8531a3e
Visan Ly <fruitdealer7@gmail.com>
2023-01-04 22:41:55 -0700

    First Commit

---
+++ public/js/tasks.js
@@ -0,0 +1,233 @@
+const deleteBtn1 = document.querySelectorAll('.del')
+const todoItem1 = document.querySelectorAll('span.not')
+const todoComplete1 = document.querySelectorAll('span.completed')
+
+Array.from(deleteBtn1).forEach((el)=>{
+    el.addEventListener('click', deleteTodo)
+})
+
+Array.from(todoItem1).forEach((el)=>{
+    el.addEventListener('click', markComplete)
+})
+
+Array.from(todoComplete1).forEach((el)=>{
+    el.addEventListener('click', markIncomplete)
+})
+
+async function deleteTodo1(){
+    const todoId = this.parentNode.dataset.id
+    try{
+        const response = await fetch('todos/deleteTodo', {
+            method: 'delete',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({
+                'todoIdFromJSFile': todoId
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    }catch(err){
+        console.log(err)
+    }
+}
+
+async function markComplete1(){
+    const todoId = this.parentNode.dataset.id
+    try{
+        const response = await fetch('todos/markComplete', {
+            method: 'put',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({
+                'todoIdFromJSFile': todoId
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    }catch(err){
+        console.log(err)
+    }
+}
+
+async function markIncomplete1(){
+    const todoId = this.parentNode.dataset.id
+    try{
+        const response = await fetch('todos/markIncomplete', {
+            method: 'put',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({
+                'todoIdFromJSFile': todoId
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    }catch(err){
+        console.log(err)
+    }
+}
+
+
+
+
+var pomodoro = {
+    started : false,
+    minutes : 0,
+    seconds : 0,
+    fillerHeight : 0,
+    fillerIncrement : 0,
+    interval : null,
+    minutesDom : null,
+    secondsDom : null,
+    fillerDom : null,
+    init : function(){
+      var self = this;
+      this.minutesDom = document.querySelector('#minutes');
+      this.secondsDom = document.querySelector('#seconds');
+      this.fillerDom = document.querySelector('#filler');
+      this.interval = setInterval(function(){
+        self.intervalCallback.apply(self);
+      }, 1000);
+      document.querySelector('#work').onclick = function(){
+        self.startWork.apply(self);
+        document.body.style.backgroundColor = '#800080'
+      };
+      document.querySelector('#shortBreak').onclick = function(){
+        self.startShortBreak.apply(self);
+        document.body.style.backgroundColor =  '#008080'
+      };
+      document.querySelector('#longBreak').onclick = function(){
+        self.startLongBreak.apply(self);
+        document.body.style.backgroundColor =  '#008080'
+      };
+      document.querySelector('#stop').onclick = function(){
+        self.stopTimer.apply(self);
+        document.body.style.backgroundColor = 'white'
+      };
+    },
+    resetVariables : function(mins, secs, started){
+      this.minutes = mins;
+      this.seconds = secs;
+      this.started = started;
+      this.fillerIncrement = 200/(this.minutes*60);
+      this.fillerHeight = 0;  
+    },
+    startWork: function() {
+      this.resetVariables(25, 0, true);
+    },
+    startShortBreak : function(){
+      this.resetVariables(5, 0, true);
+    },
+    startLongBreak : function(){
+      this.resetVariables(15, 0, true);
+    },
+    stopTimer : function(){
+      this.resetVariables(25, 0, false);
+      this.updateDom();
+    },
+    toDoubleDigit : function(num){
+      if(num < 10) {
+        return "0" + parseInt(num, 10);
+      }
+      return num;
+    },
+    updateDom : function(){
+      this.minutesDom.innerHTML = this.toDoubleDigit(this.minutes);
+      this.secondsDom.innerHTML = this.toDoubleDigit(this.seconds);
+      this.fillerHeight = this.fillerHeight + this.fillerIncrement;
+      this.fillerDom.style.height = this.fillerHeight + 'px';
+    },
+    intervalCallback : function(){
+      if(!this.started) return false;
+      if(this.seconds == 0) {
+        if(this.minutes == 0) {
+          this.timerComplete();
+          return;
+        }
+        this.seconds = 59;
+        this.minutes--;
+      } else {
+        this.seconds--;
+      }
+      this.updateDom();
+    },
+    timerComplete : function(){
+      this.started = false;
+      this.fillerHeight = 0;
+    }
+};
+window.onload = function(){
+  pomodoro.init();
+};
+
+
+const deleteBtn = document.querySelectorAll('.del')
+const todoItem = document.querySelectorAll('span.not')
+const todoComplete = document.querySelectorAll('span.completed')
+
+Array.from(deleteBtn).forEach((el)=>{
+    el.addEventListener('click', deleteTodo)
+})
+
+Array.from(todoItem).forEach((el)=>{
+    el.addEventListener('click', markComplete)
+})
+
+Array.from(todoComplete).forEach((el)=>{
+    el.addEventListener('click', markIncomplete)
+})
+
+async function deleteTodo(){
+    const todoId = this.parentNode.dataset.id
+    try{
+        const response = await fetch('todos/deleteTodo', {
+            method: 'delete',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({
+                'todoIdFromJSFile': todoId
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    }catch(err){
+        console.log(err)
+    }
+}
+
+async function markComplete(){
+    const todoId = this.parentNode.dataset.id
+    try{
+        const response = await fetch('todos/markComplete', {
+            method: 'put',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({
+                'todoIdFromJSFile': todoId
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    }catch(err){
+        console.log(err)
+    }
+}
+
+async function markIncomplete(){
+    const todoId = this.parentNode.dataset.id
+    try{
+        const response = await fetch('todos/markIncomplete', {
+            method: 'put',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({
+                'todoIdFromJSFile': todoId
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    }catch(err){
+        console.log(err)
+    }
+}
