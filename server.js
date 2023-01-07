09f009d4b240a8e3e4b8224df185a036100a96a3
Visan Ly <fruitdealer7@gmail.com>
2023-01-06 18:11:31 -0700

    Testing connection changes

--- server.js
+++ server.js
@@ -64,11 +64,6 @@ app.use('/todos', todoRoutes)
 
 
 //Server Running
-connectDB().then(() => {
-  app.listen(process.env.PORT, () => {
-    console.log("Server is running, you better catch it!");
-  })
-})
-/*app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server is running, you better catch it!");
-});*/
+});
